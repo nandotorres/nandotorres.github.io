bb88177e57af6e4bd862d29a770837c3cb067192
nandotorres <nandotorres@gmail.com>
2014-07-16 19:55:54 -0300

    Adicionando google analytics

---
+++ commitar.sh
@@ -0,0 +1,17 @@
+git add .
+git commit -am "$1"
+echo "Gerar o site?"
+select yn in "Yes" "No"; do
+    case $yn in
+       Yes ) jekyll build; break;;
+       No ) break;;
+    esac
+done
+
+echo "Atualizar o blog no github?"
+select yn in "Yes" "No"; do
+  case $yn in 
+    Yes ) cd _site; git add .; git commit -am "$1"; push origin $2; cd ..;break;;
+    No ) break;;
+  esac
+done
